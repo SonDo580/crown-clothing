bfa857fdab0bda3dbcccdf67068bf9cb0e746cca
Son Do <dominhson580@gmail.com>
2023-05-07 21:46:47 +0700

    Add a NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <NavBar />
+
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
