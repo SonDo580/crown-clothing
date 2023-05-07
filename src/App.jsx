6bb8e493c890c143acfb41c04f642dba20c722bb
Son Do <dominhson580@gmail.com>
2023-05-07 21:29:01 +0700

    Add Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Categories from "./components/Categories";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <>
-      <Categories />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
