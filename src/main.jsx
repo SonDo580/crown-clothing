46e2bfbb121c2599a382aade259d69c52c5f504c
Son Do <dominhson580@gmail.com>
2023-05-07 21:41:24 +0700

    Use BrowserRouter in App

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import "./main.scss";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
