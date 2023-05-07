931ff324c9883129e6731ee9aec94d2faf482228
Son Do <dominhson580@gmail.com>
2023-05-07 17:11:27 +0700

    Add Roboto font

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./main.scss";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
