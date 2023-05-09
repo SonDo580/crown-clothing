825a4cd354079de799b1b154cc82abc3d2aa7f30
Son Do <dominhson580@gmail.com>
2023-05-09 22:09:25 +0700

    Handle changing inputs

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 export default function SignUpForm() {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields((fields) => ({
+      ...fields,
+      [name]: value,
+    }));
+  };
+
   const onSubmit = () => {};
 
   return (
@@ -6,16 +28,40 @@ export default function SignUpForm() {
       <h1>Sign up with your email and password</h1>
       <form onSubmit={onSubmit}>
         <label>Display Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          name="displayName"
+          value={displayName}
+          onChange={onChange}
+        />
 
         <label>Email</label>
-        <input type="email" required />
+        <input
+          type="email"
+          required
+          name="email"
+          value={email}
+          onChange={onChange}
+        />
 
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={onChange}
+        />
 
         <button type="submit">Sign Up</button>
       </form>
