bed6f7781ab22f9afe6d1b24d053aad0e690d040
Son Do <dominhson580@gmail.com>
2023-05-10 22:55:35 +0700

    Create custom button

---
+++ src/common/Button/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+const buttonTypeClasses = {
+  google: "google-sign-in",
+  inverted: "inverted",
+};
+
+export default function Button({ children, buttonType, ...otherProps }) {
+  return (
+    <button
+      className={`button-container ${buttonTypeClasses[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  buttonType: PropTypes.string.isRequired,
+};
