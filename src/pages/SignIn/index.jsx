05b5f8139bbf4aaafbd5514a48a040561f206d48
Son Do <dominhson580@gmail.com>
2023-05-10 21:55:30 +0700

    Add Signup Form

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import SignUpForm from "../../components/SignUpForm";
 import {
   createUserDocument,
   signInWithGoogle,
@@ -8,12 +9,16 @@ export default function SignIn() {
     const { user } = await signInWithGoogle();
 
     const userDocRef = await createUserDocument(user);
+
+    console.log(userDocRef);
   };
 
   return (
     <div>
       <h1>SignIn Page</h1>
       <button onClick={googleSignIn}>Sign in with Google</button>
+
+      <SignUpForm />
     </div>
   );
 }
