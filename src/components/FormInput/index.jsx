f21c17323b2ff2c4ac8431cee488a3f063b02aa8
Son Do <dominhson580@gmail.com>
2023-05-10 22:33:04 +0700

    Style FormInput

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,14 +1,24 @@
 import PropTypes from "prop-types";
+import "./formInput.scss";
 
 export default function FormInput({ label, ...otherProps }) {
   return (
-    <div>
-      <label>{label}</label>
-      <input {...otherProps} />
+    <div className="group">
+      <input className="form-input" {...otherProps} />
+
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 }
 
 FormInput.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
