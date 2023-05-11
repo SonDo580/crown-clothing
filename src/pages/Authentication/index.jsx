729dcd7df4ee6798e27200a967e89b88bba7baa0
Son Do <dominhson580@gmail.com>
2023-05-11 22:31:34 +0700

    Add SignInForm and style the page

--- src/pages/Authentication/index.jsx
+++ src/pages/Authentication/index.jsx
@@ -1,30 +1,11 @@
-import { toast } from "react-toastify";
-
-import {
-  createUserDocument,
-  signInWithGoogle,
-} from "../../utils/firebase.utils";
-
+import "./authentication.scss";
+import SignInForm from "../../components/SignInForm";
 import SignUpForm from "../../components/SignUpForm";
 
 export default function Authentication() {
-  const googleSignIn = async () => {
-    try {
-      const { user } = await signInWithGoogle();
-
-      const userDocRef = await createUserDocument(user);
-
-      console.log(userDocRef);
-    } catch (err) {
-      toast.error(err.message);
-    }
-  };
-
   return (
-    <div>
-      <h1>Authentication Page</h1>
-      <button onClick={googleSignIn}>Sign in with Google</button>
-
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
