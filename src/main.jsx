b59d8b0e31208f1134a6bcd6752a3af627222cf3
Son Do <dominhson580@gmail.com>
2023-05-14 21:39:37 +0700

    Wrap the App in UserProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./main.scss";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App.jsx";
+import { UserProvider } from "./contexts/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
