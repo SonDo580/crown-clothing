1a268e2f084da547d7ac1fb3f5884d22c02f000b
Son Do <dominhson580@gmail.com>
2023-05-17 21:57:37 +0700

    Update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import NavBar from "./components/NavBar";
