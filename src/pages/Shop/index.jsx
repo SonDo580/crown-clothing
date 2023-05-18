ec3ced2b49808a6be3ed018e32c4db070cd85d0a
Son Do <dominhson580@gmail.com>
2023-05-18 22:11:12 +0700

    Render list of Product Cards

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
+
 import { ProductsContext } from "../../contexts/ProductsContext";
+import ProductCard from "../../components/ProductCard";
 
 export default function Shop() {
   const products = useContext(ProductsContext);
 
   return (
     <div>
-      {products.map(({ id, name }) => (
-        <h1 key={id}>{name}</h1>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
