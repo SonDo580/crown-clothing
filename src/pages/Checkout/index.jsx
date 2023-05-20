b76ce5c39df257f2a819148d7f5db37234050122
Son Do <dominhson580@gmail.com>
2023-05-20 16:12:17 +0700

    Test increment and decrement functions

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,3 +1,26 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/CartContext";
+
 export default function Checkout() {
-  return <div>Checkout Page</div>;
+  const { cartItems, incrementItemQuantity, decrementItemQuantity } =
+    useContext(CartContext);
+
+  // TODO: navigate to shop if cart has no items
+
+  return (
+    <div>
+      <h1>Checkout Page</h1>
+      <div>
+        {cartItems.map(({ name, id, quantity }) => (
+          <div key={id}>
+            <h2>{name}</h2>
+            <button onClick={() => decrementItemQuantity(id)}>-</button>
+            <span>{quantity}</span>
+            <button onClick={() => incrementItemQuantity(id)}>+</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
