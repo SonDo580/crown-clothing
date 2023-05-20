b697c3be9b73e2b35b459c83babc21ed44a843c4
Son Do <dominhson580@gmail.com>
2023-05-20 10:36:01 +0700

    Show number of products in Cart

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -6,12 +6,12 @@ import "./cartIcon.scss";
 import { ReactComponent as ShoppingBag } from "../../assets/shopping-bag.svg";
 
 export default function CartIcon() {
-  const { toggleCartVisible } = useContext(CartContext);
+  const { cartItems, toggleCartVisible } = useContext(CartContext);
 
   return (
     <div className="cart-icon-container" onClick={toggleCartVisible}>
       <ShoppingBag className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartItems.length}</span>
     </div>
   );
 }
