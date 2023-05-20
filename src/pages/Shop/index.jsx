cbe1b6418998c1ac8caa59c081a544e7625bc3c4
Son Do <dominhson580@gmail.com>
2023-05-20 16:20:56 +0700

    Navigate to login page if user hasn't logged in

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,11 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { UserContext } from "../../contexts/UserContext";
 import { ProductsContext } from "../../contexts/ProductsContext";
 
 import "./shop.scss";
 import ProductCard from "../../components/ProductCard";
 
 export default function Shop() {
+  const navigate = useNavigate();
+
+  const currentUser = useContext(UserContext);
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/auth");
+    }
+  }, [currentUser, navigate]);
+
   const products = useContext(ProductsContext);
 
   return (
