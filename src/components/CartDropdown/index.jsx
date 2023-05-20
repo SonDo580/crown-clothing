edce135fc1d21aa120a22fda68be57fe2dee640a
Son Do <dominhson580@gmail.com>
2023-05-20 10:03:40 +0700

    Render the list of CartItem

--- src/components/CartDropdown/index.jsx
+++ src/components/CartDropdown/index.jsx
@@ -1,10 +1,21 @@
-import Button from "../../common/Button";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/CartContext";
+
 import "./cartDropdown.scss";
+import Button from "../../common/Button";
+import CartItem from "../CartItem";
 
 export default function CartDropdown() {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>Go to Checkout</Button>
     </div>
   );
