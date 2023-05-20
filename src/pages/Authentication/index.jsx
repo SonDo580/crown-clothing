713e533f073cfb29753c85b6cc093dc43dcaea84
Son Do <dominhson580@gmail.com>
2023-05-20 16:39:53 +0700

    Navigate to previous path if login successful

--- src/pages/Authentication/index.jsx
+++ src/pages/Authentication/index.jsx
@@ -1,8 +1,26 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+
+import { UserContext } from "../../contexts/UserContext";
+
 import "./authentication.scss";
 import SignInForm from "../../components/SignInForm";
 import SignUpForm from "../../components/SignUpForm";
 
 export default function Authentication() {
+  const navigate = useNavigate();
+
+  const { state: locationState } = useLocation();
+  const prevPath = (locationState && locationState.prevPath) || "/";
+
+  const currentUser = useContext(UserContext);
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate(prevPath);
+    }
+  }, [currentUser, prevPath, navigate]);
+
   return (
     <div className="authentication-container">
       <SignInForm />
