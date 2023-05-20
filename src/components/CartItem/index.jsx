a10abec7c983170bf3ab1be1fb8633c7148e339a
Son Do <dominhson580@gmail.com>
2023-05-20 12:04:42 +0700

    Add a remove button

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -1,8 +1,15 @@
+import { useContext } from "react";
 import PropTypes from "prop-types";
+
+import { CartContext } from "../../contexts/CartContext";
 import "./cartItem.scss";
 
 export default function CartItem({ cartItem }) {
-  const { name, imageUrl, price } = cartItem;
+  const { id, name, imageUrl, price } = cartItem;
+
+  const { removeProductFromCart } = useContext(CartContext);
+
+  const removeFromCart = () => removeProductFromCart(id);
 
   return (
     <div className="cart-item-container">
@@ -12,12 +19,17 @@ export default function CartItem({ cartItem }) {
         <h2 className="name">{name}</h2>
         <span>${price}</span>
       </div>
+
+      <div className="remove-button">
+        <button onClick={removeFromCart}>X</button>
+      </div>
     </div>
   );
 }
 
 CartItem.propTypes = {
   cartItem: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
