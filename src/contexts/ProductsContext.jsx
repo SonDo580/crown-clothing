68986b1c6d322fc6f5cfd2043bf30c303bdb1bab
Son Do <dominhson580@gmail.com>
2023-05-26 22:37:22 +0700

    Add SHOP_DATA to database

--- src/contexts/ProductsContext.jsx
+++ src/contexts/ProductsContext.jsx
@@ -1,12 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import shopData from "../mock/shopData.json";
+import { addCollectionAndDocuments } from "../utils/firebase.utils";
+import { SHOP_DATA } from "../data/shopData";
 
 export const ProductsContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(shopData);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);
 
   return (
     <ProductsContext.Provider value={products}>
