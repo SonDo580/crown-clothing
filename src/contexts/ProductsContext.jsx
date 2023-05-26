91e044e5bd7b1fafbfcbe6266268251f2423cbbb
Son Do <dominhson580@gmail.com>
2023-05-26 22:41:35 +0700

    Remove add-data-to-database code

--- src/contexts/ProductsContext.jsx
+++ src/contexts/ProductsContext.jsx
@@ -1,17 +1,10 @@
 import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import { addCollectionAndDocuments } from "../utils/firebase.utils";
-import { SHOP_DATA } from "../data/shopData";
-
 export const ProductsContext = createContext();
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    addCollectionAndDocuments("categories", SHOP_DATA);
-  }, []);
 
   return (
     <ProductsContext.Provider value={products}>
