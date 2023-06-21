d346be9d9d93564926c1386f4aeb08f2f5076259
Son Do <dominhson580@gmail.com>
2023-06-21 08:16:21 +0700

    Add Spinner component

---
+++ src/common/Spinner/index.jsx
@@ -0,0 +1,9 @@
+import { SpinnerContainer, SpinnerOverlay } from "./spinner.style";
+
+export default function Spinner() {
+  return (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  );
+}
