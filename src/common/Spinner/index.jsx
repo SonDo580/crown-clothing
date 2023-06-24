6acee343d672dbfd9ccf0b9eb185855f439a035f
Son Do <dominhson580@gmail.com>
2023-06-24 21:57:37 +0700

    Add mode 'fullScreen' for Spinner

--- src/common/Spinner/index.jsx
+++ src/common/Spinner/index.jsx
@@ -1,9 +1,18 @@
+import PropTypes from "prop-types";
 import { SpinnerContainer, SpinnerOverlay } from "./spinner.style";
 
-export default function Spinner() {
+export default function Spinner({ fullScreen }) {
   return (
-    <SpinnerOverlay>
+    <SpinnerOverlay fullScreen={fullScreen}>
       <SpinnerContainer />
     </SpinnerOverlay>
   );
 }
+
+Spinner.defaultProps = {
+  fullScreen: false,
+};
+
+Spinner.propTypes = {
+  fullScreen: PropTypes.bool,
+};
--- src/common/Spinner/spinner.style.jsx
+++ src/common/Spinner/spinner.style.jsx
@@ -1,4 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const fullOverlayStyle = css`
+  position: fixed;
+  top: 0;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 100;
+`;
 
 export const SpinnerOverlay = styled.div`
   height: 60vh;
@@ -6,6 +14,8 @@ export const SpinnerOverlay = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  ${({ fullScreen }) => fullScreen && fullOverlayStyle}
 `;
 
 export const SpinnerContainer = styled.div`
