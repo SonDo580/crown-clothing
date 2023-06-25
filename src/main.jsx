79600970bcb60b1ca915134d8976f79a298a936b
Son Do <dominhson580@gmail.com>
2023-06-25 11:46:22 +0700

    Add redux toolkit configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
-import { persistor, store } from "./redux/store";
+// import { persistor, store } from "./redux/store";
+import { store } from "./redux/store";
+
 import "./main.scss";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,30 @@
-import { applyMiddleware, compose, createStore } from "redux";
-// import thunk from "redux-thunk";
-// import logger from "redux-logger";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["cart"],
+// };
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = createStore(persistedReducer, undefined, composeEnhancers);
+// export const persistor = persistStore(store);
+
+import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 
-import { rootSaga } from "./rootSaga";
 import { rootReducer } from "./rootReducer";
+import { rootSaga } from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
 
-const middlewares = [
-  // process.env.NODE_ENV !== "production" && logger,
-  // thunk,
-  sagaMiddleware,
-].filter(Boolean);
-
-const composeEnhancer =
-  (process.env.NODE_ENV !== "production" &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composeEnhancers = composeEnhancer(applyMiddleware(...middlewares));
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["cart"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = createStore(persistedReducer, undefined, composeEnhancers);
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: middlewares,
+});
 
 sagaMiddleware.run(rootSaga);
-
-export const persistor = persistStore(store);
